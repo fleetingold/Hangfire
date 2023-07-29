603c83696af77173d612d283cd1cbe22ac15a53f
fleetingold <1161851523@qq.com>
2023-07-29 16:40:43 +0800

    ConsoleSample - BackgroundJobServer

--- samples/ConsoleSample/Program.cs
+++ samples/ConsoleSample/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace ConsoleSample
+﻿using Hangfire;
+
+namespace ConsoleSample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            GlobalConfiguration.Configuration
+                .SetDataCompatibilityLevel(CompatibilityLevel.Version_180)
+                .UseColouredConsoleLogProvider()
+                .UseSimpleAssemblyNameTypeSerializer()
+                .UseRecommendedSerializerSettings()
+                .UseSqlServerStorage(@"server=192.168.1.148;database=OMSServerTimedTask;uid=sa;pwd=ABC!@#abc123");
+
+            using var server = new BackgroundJobServer();
+            Console.ReadLine();
         }
     }
 }
